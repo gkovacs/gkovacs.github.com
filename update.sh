1764c9f8bf670616c38d1d610a06566d0685288c
Geza Kovacs <gkovacs@mit.edu>
2012-09-18 16:45:19 -0400

    updated site

--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 pdflatex resume.tex
+#latex resume.tex
+#catdvi -e 0 -U resume.dvi | sed -re "s/([^^[:space:]])\s+/\1 /g" > resume.txt
+latex2html -html_version 4.0,latin1,unicode -split 1 -nonavigation -noinfo -noaddress -title "Geza Kovacs Resume" -dir resume-html -mkdir resume.tex
+lynx resume-html/index.html -dump -nolist > resume.txt
 git commit -a -m "updated site"
 git push origin master
